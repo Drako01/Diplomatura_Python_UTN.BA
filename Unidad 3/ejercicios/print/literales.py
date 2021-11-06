4fd5fcf41449caab24557856bc1a3dc66551d690
Alejandro Daniel Di Stefano <88512335+Drako01@users.noreply.github.com>
2021-11-06 14:56:35 -0300

    06112021

--- Unidad 3/ejercicios/print/literales.py
+++ Unidad 3/ejercicios/print/literales.py
@@ -8,7 +8,6 @@ valor1 = 'Primer participante'
 valor2 = 'Anna'
 valor3=''
 print(f'{valor1:30}==>{valor3:10}{valor2}')
-
 print('------------------------------------------')
 votos = 42572654
 voto_en_blanco = 43132495
@@ -18,4 +17,4 @@ print('------------------------------------------')
 votos = 42572654
 voto_en_blanco = 43132495
 porcentaje = (votos / (votos + voto_en_blanco)) *100
-print('Los votos a favor son: {0} con un porcentaje de:   {1:.2f}'.format(votos = , porcentaje))
+print('Los votos a favor son: {0} con un porcentaje de:   {1:.2f}'.format(votos =  porcentaje))
