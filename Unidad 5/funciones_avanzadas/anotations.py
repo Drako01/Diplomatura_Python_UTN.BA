ffb77f99da5f2e5fe369d9cd5ea818882e78e7d9
Alejandro Daniel Di Stefano <88512335+Drako01@users.noreply.github.com>
2021-11-17 22:14:00 -0300

    17112021

--- Unidad 5/funciones_avanzadas/anotations.py
+++ Unidad 5/funciones_avanzadas/anotations.py
@@ -3,6 +3,9 @@
 # ##############################################
 
 
+from typing import Tuple
+
+
 def func1(a, b, c):
     return a + b + c
 
@@ -10,7 +13,7 @@ print(func1(1, 2, 3))
 print('',end='\n################\n' )
 
 
-def func2(a: 'spam', b: (1, 10), c: float) -> int:
+def func2(a: str, b: Tuple, c: float) -> int:
     return a + b + c
 
 
@@ -19,7 +22,7 @@ print(func2.__annotations__)
 print('',end='\n################\n' )
 
 
-def func3(a: 'spam' = 4, b: (1, 10) = 5, c: float = 6) -> int:
+def func3(a: str = 4, b: Tuple = 5, c: float = 6) -> int:
     return a + b + c
 
 print(func3(1, 2, 3))
